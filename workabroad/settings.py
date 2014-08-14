cd1a7daf7fec8625559e172bf15b73eedbc8b102
staceytay <stay.jw@gmail.com>
2014-08-14 16:26:04 +0800

    Initial commit

---
+++ workabroad/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for workabroad project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'workabroad'
+
+SPIDER_MODULES = ['workabroad.spiders']
+NEWSPIDER_MODULE = 'workabroad.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'workabroad (+http://www.yourdomain.com)'
